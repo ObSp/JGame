50c7ba23639ecb7fedb1c36df463e479091d1306
ObSp <150492054+ObSp@users.noreply.github.com>
2024-05-25 10:50:36 -0400

    h

---
+++ MarshmallowFighter/Classes/House.java
@@ -0,0 +1,14 @@
+package MarshmallowFighter.Classes;
+
+import JGamePackage.JGame.JGame;
+import JGamePackage.JGame.Instances.Image2D;
+
+public class House extends Model {
+
+    public House(JGame game) {
+        super(game);
+        
+        model = new Image2D();
+    }
+    
+}
